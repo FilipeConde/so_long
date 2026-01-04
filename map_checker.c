2ed68a7090314f44c8dc4102e8418cb9c8634bf4
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2026-01-04 17:48:57 -0300

    feat: check if map is closed.

--- map_checker.c
+++ map_checker.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 13:52:56 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/04 14:31:42 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/04 17:48:25 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,41 @@ static int	is_rectangle(char **map)
 	return (EXIT_SUCCESS);
 }
 
+static int	is_closed(char **map)
+{
+	int	lines_qty;
+	int	line_size;
+	int	i;
+
+	if (!map || !*map)
+		return (-1);
+	lines_qty = 0;
+	while (map[lines_qty])
+		lines_qty++;
+	i = 0;
+	line_size = ft_strlen(map[0]);
+	while (i < line_size)
+	{
+		if (map[0][i] != '1' || map[lines_qty - 1][i] != '1' )
+			return (EXIT_FAILURE);
+		i++;
+	}
+	i = 0;
+	while (i < lines_qty)
+	{
+		if (map[i][0] != '1' || map[i][line_size - 1] != '1' )
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
 int	map_checker(char **map)
 {
-	if (!is_rectangle(map))
+	if (is_rectangle(map) != 0)
 		return (EXIT_FAILURE);
-	// is_closed(**map);
+	if (is_closed(map) != 0)
+		return (EXIT_FAILURE);
 	// has_entities(**map);
 
 	return (EXIT_SUCCESS);
