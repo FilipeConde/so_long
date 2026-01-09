fa71dd4981d73aad4af494e80d0a413120574fa9
Filipe Conde Pereira <fconde-p@c1r1p3.42sp.org.br>
2026-01-08 23:54:14 -0300

    chore: set logic for flood fill.

--- flood_fill.c
+++ flood_fill.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   flood_fill.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 20:50:11 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/05 20:51:19 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/08 23:51:46 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	flood_fill(t_board *board)
 {
-	
+	// create copy of map
+	// start by player
+	// check up, down, left and right
+	// if different than '1' then set to '1' and do floodfill again
+	// if '1', then go to the next
+	// at the very end, check map for entities ('P', 'C', 'E')
+	// 
 }
