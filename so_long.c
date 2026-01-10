1bc62d1bf22b1d233e571bab40860d7ac1d7b154
Filipe Conde <fconde-p@student.42sp.org.br>
2026-01-10 19:52:32 -0300

    chore: remove comments from file.

--- so_long.c
+++ so_long.c
@@ -6,12 +6,12 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 16:54:24 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/10 16:21:26 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/10 19:52:10 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-// code as hell!!!
+
 int	main(int argc, char **argv)
 {
 	t_board	*board;
@@ -26,23 +26,7 @@ int	main(int argc, char **argv)
 		free(board);
 		return (EXIT_FAILURE);
 	}
-	ft_printf("QTY C: %d\n", board->c_qty);
-	ft_printf("POS P:%d %d\n", board->p_x, board->p_y);
-	ft_printf("POS E:%d %d\n", board->e_x, board->e_y);
 	i = 0;
-	// while (board->map[i])
-	// {
-	// 	ft_printf("%s\n", board->map[i]);
-	// 	i++;
-	// }
-
-	// ft_printf("%d\n", map_checker(board->map));
-	// if (map_checker(board->map) != 0)
-	// {
-	// 	free_board(board);
-	// 	ft_printf("Invalid board\n");
-	// 	return (EXIT_FAILURE);
-	// }
 	free_board(board);
 	return (EXIT_SUCCESS);
 }
