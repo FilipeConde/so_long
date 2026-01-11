f7be897b6b076217919da78e1ec91c64bbad45ce
Filipe Conde <fconde-p@student.42sp.org.br>
2026-01-11 19:11:58 -0300

    feat: update process to create graphic elements and window.

--- mount_window.c
+++ mount_window.c
@@ -6,25 +6,23 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 19:52:54 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/11 16:27:17 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/11 18:54:01 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int32_t	mount_window(t_board *board)
+int32_t	mount_window(t_board *board, t_game *game)
 {
-	mlx_t* mlx = mlx_init((board->width * TILE),
+	game->mlx = mlx_init((board->width * TILE),
 		(board->height * TILE), "Test", true);
-	// Try to load the file
-	mlx_texture_t* texture = mlx_load_png("./assets/vampire.png");
-	// Convert texture to a displayable image
-	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
-	// Display the image
-	mlx_image_to_window(mlx, img, 2 * TILE, 2 * TILE);
-	mlx_loop(mlx);
-	mlx_delete_image(mlx, img);
-	mlx_delete_texture(texture);
-	mlx_terminate(mlx);
+	set_sprites(game->mlx, game);
+	mlx_image_to_window(game->mlx, game->img_p, 2 * TILE, 2 * TILE);
+	mlx_image_to_window(game->mlx, game->img_c, 3 * TILE, 3 * TILE);
+	mlx_loop(game->mlx);
+	delete_images(game);
+	// mlx_delete_image(game->mlx, img);
+	// mlx_delete_texture(texture);
+	mlx_terminate(game->mlx);
 	return (EXIT_SUCCESS);
 }
