0708a5c965d78e81502a2819aca1ec4411482140
Filipe Conde <fconde-p@student.42sp.org.br>
2026-01-11 14:54:09 -0300

    chore: generate sample of image on window using assets.

--- mount_window.c
+++ mount_window.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 19:52:54 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/10 20:00:34 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/11 14:37:55 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 int32_t	mount_window(void)
 {
 	mlx_t* mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "Test", true);
+
+	// Try to load the file
+	mlx_texture_t* texture = mlx_load_png("./assets/skull.png");
+	// Convert texture to a displayable image
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	// Display the image
+	mlx_image_to_window(mlx, img, 32, 32);
 	mlx_loop(mlx);
-	// mlx_delete_image(mlx, img);
-	// mlx_delete_xpm42(xpm);
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
