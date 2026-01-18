4f01ab2cae1039b3eca523d7f7851cdfed638ce1
Filipe Conde <fconde-p@student.42sp.org.br>
2026-01-18 20:41:25 -0300

    fix: solve issue of phantom collectable.

--- src/graphics/do_collect.c
+++ src/graphics/do_collect.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 16:36:32 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/18 20:15:33 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/18 20:39:53 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ void	do_collect(t_game *game)
 	while (i <= game->board->c_total)
 	{
 		if (game->img_c->instances[i].x == game->img_p->instances[0].x
-			&& game->img_c->instances[i].y == game->img_p->instances[0].y)
+			&& game->img_c->instances[i].y == game->img_p->instances[0].y
+			&& game->img_c->instances[i].enabled == true)
+		{
 			game->img_c->instances[i].enabled = false;
+			game->board->c_qty--;
+			if (game->board->c_qty == 0)
+				game->img_e->enabled = true;
+			break ;
+		}
 		i++;
 	}
-	game->board->c_qty--;
-	if (game->board->c_qty == 0)
-		game->img_e->enabled = true;
 }
